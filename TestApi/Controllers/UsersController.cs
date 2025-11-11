d44a8f462cb1a88d58a14529867ab908e95ec96f
Suraj Kewat <surajkewat03@gmail.com>
2025-11-11 10:40:57 +0530

    Add .gitignore to ignore VS and temp files

---
+++ TestApi/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestApi.Data;
+using TestApi.Models;
+
+namespace TestApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+    }
+}
---
+++ TestApi/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; // ← ye add karein
+
+namespace TestApi.Models
+{
+    [Table("users")] // ← Ye ensure karega table ka naam "Users" ho
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+    }
+}
